b265dfc0052008647ca62cd17be525d4c7b5ebb1
Leonardo Ruwer <leonardo.ruwer@gmail.com>
2026-04-09 19:52:40 -0300

    Implement caching for Geocoding and Weather

--- app/services/forecasts/fetch_service.rb
+++ app/services/forecasts/fetch_service.rb
@@ -1,4 +1,6 @@
 class Forecasts::FetchService
+  CACHE_EXPIRATION = 30.minutes
+
   def self.call(location)
     new(location).call
   end
@@ -8,16 +10,16 @@ class Forecasts::FetchService
   end
 
   def call
-    geo = GeocodingService.call(@location)
+    geo = fetch_geocoding
+    return error_response(geo[:error]) unless geo[:success]
 
-    return { success: false, error: geo[:error] } unless geo[:success]
+    cached_forecast = fetch_cached_forecast(geo)
+    return cached_forecast if cached_forecast
 
-    weather = WeatherService.call(
-      latitude: geo[:latitude],
-      longitude: geo[:longitude]
-    )
+    weather = fetch_weather(geo)
+    return error_response(weather[:error]) unless weather[:success]
 
-    {
+    result = {
       location_query: @location,
       location_name: [ geo[:name], geo[:country] ].compact.join(", "),
       latitude: geo[:latitude],
@@ -29,5 +31,52 @@ class Forecasts::FetchService
       from_cache: false,
       success: true
     }
+
+    Rails.cache.write(
+      forecast_cache_key_from_coordinates(geo[:latitude], geo[:longitude]),
+      result.except(:from_cache),
+      expires_in: CACHE_EXPIRATION
+    )
+
+    result
+  end
+
+  private
+
+  def fetch_geocoding
+    Rails.cache.fetch(geocode_cache_key_from_location(@location), expires_in: CACHE_EXPIRATION) do
+      GeocodingService.call(@location)
+    end
+  end
+
+  def fetch_weather(geo)
+    WeatherService.call(
+      latitude: geo[:latitude],
+      longitude: geo[:longitude]
+    )
+  end
+
+  def forecast_cache_key_from_coordinates(latitude, longitude)
+    lat = format("%.4f", latitude)
+    lon = format("%.4f", longitude)
+
+    "forecast:#{lat}:#{lon}"
+  end
+
+  def fetch_cached_forecast(geo)
+    cached_data = Rails.cache.read(
+      forecast_cache_key_from_coordinates(geo[:latitude], geo[:longitude])
+    )
+
+    cached_data&.merge(from_cache: true)
+  end
+
+  def geocode_cache_key_from_location(location)
+    normalized_location = location.to_s.strip.downcase
+    "geocode:#{normalized_location}"
+  end
+
+  def error_response(message)
+    { success: false, error: message }
   end
 end
