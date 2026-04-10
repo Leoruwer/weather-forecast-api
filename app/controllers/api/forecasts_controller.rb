5c1932e38ded2e640894239a72982a9b99c3ffe0
Leonardo Ruwer <leonardo.ruwer@gmail.com>
2026-04-10 14:25:44 -0300

    Add success camp on failure responses

--- app/controllers/api/forecasts_controller.rb
+++ app/controllers/api/forecasts_controller.rb
@@ -2,11 +2,11 @@ class Api::ForecastsController < ApplicationController
   def index
     name = params[:name]
 
-    return render json: { error: "Name parameter is required" }, status: :bad_request if name.blank?
+    return render json: { success: false, error: "Name parameter is required" }, status: :bad_request if name.blank?
 
     result = Forecasts::FetchService.call(name)
 
     return render json: ForecastSerializer.new(result).as_json, status: :ok if result[:success]
-    render json: { error: result[:error] }, status: :bad_request
+    render json: { success: false, error: result[:error] }, status: :bad_request
   end
 end
