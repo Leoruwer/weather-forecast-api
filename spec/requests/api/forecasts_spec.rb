edbca40ad2c1f6090aba0352acf8e943b19274f0
Leonardo Ruwer <leonardo.ruwer@gmail.com>
2026-05-12 18:47:57 -0300

    Improve forecasts specs to use shared_examples

--- spec/requests/api/forecasts_spec.rb
+++ spec/requests/api/forecasts_spec.rb
@@ -66,105 +66,13 @@ RSpec.describe "Api::Forecasts", type: :request do
       context "when location is zip code" do
         let(:geo_params) { { name: "10001" } }
 
-        it "returns status ok" do
-          subject
-
-          expect(response).to have_http_status(:ok)
-        end
-
-        it "returns json content type" do
-          subject
-
-          expect(response.content_type).to eq("application/json; charset=utf-8")
-        end
-
-        it "returns the forecast location data" do
-          subject
-
-          body = JSON.parse(response.body)
-
-          expect(body["data"]["location"]).to include(
-            "location_name" => "New York, United States",
-            "latitude" => 40.7128,
-            "longitude" => -74.0060
-          )
-        end
-
-        it "returns the forecast data" do
-          subject
-
-          body = JSON.parse(response.body)
-
-          expect(body["data"]).to include(
-            "current_temperature" => 23,
-            "high_temperature" => 30,
-            "low_temperature" => 15,
-            "extended_forecast" => [
-              { "date" => "2026-04-10", "high_temperature" => 31, "low_temperature" => 16 },
-              { "date" => "2026-04-11", "high_temperature" => 32, "low_temperature" => 17 },
-              { "date" => "2026-04-12", "high_temperature" => 33, "low_temperature" => 18 }
-            ],
-            "from_cache" => false
-          )
-        end
-
-        it "calls the fetch service" do
-          subject
-
-          expect(Forecasts::FetchService).to have_received(:call).with("10001")
-        end
+        it_behaves_like "a successful forecast response", "10001"
       end
 
       context "when location is city name" do
         let(:geo_params) { { name: "New York" } }
 
-        it "returns status ok" do
-          subject
-
-          expect(response).to have_http_status(:ok)
-        end
-
-        it "returns json content type" do
-          subject
-
-          expect(response.content_type).to eq("application/json; charset=utf-8")
-        end
-
-        it "returns the forecast location data" do
-          subject
-
-          body = JSON.parse(response.body)
-
-          expect(body["data"]["location"]).to include(
-            "location_name" => "New York, United States",
-            "latitude" => 40.7128,
-            "longitude" => -74.0060
-          )
-        end
-
-        it "returns the forecast data" do
-          subject
-
-          body = JSON.parse(response.body)
-
-          expect(body["data"]).to include(
-            "current_temperature" => 23,
-            "high_temperature" => 30,
-            "low_temperature" => 15,
-            "extended_forecast" => [
-              { "date" => "2026-04-10", "high_temperature" => 31, "low_temperature" => 16 },
-              { "date" => "2026-04-11", "high_temperature" => 32, "low_temperature" => 17 },
-              { "date" => "2026-04-12", "high_temperature" => 33, "low_temperature" => 18 }
-            ],
-            "from_cache" => false
-          )
-        end
-
-        it "calls the fetch service" do
-          subject
-
-          expect(Forecasts::FetchService).to have_received(:call).with("New York")
-        end
+        it_behaves_like "a successful forecast response", "New York"
       end
     end
 
